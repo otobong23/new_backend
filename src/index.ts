9fc38293875cd5b15370af2ea6ff12ca4e65346a
miracle boniface <udohmiracle23@gmail.com>
2025-08-20 19:54:24 +0100

    repo updated

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
+import http from 'http';
+import mongoose from 'mongoose';
+
